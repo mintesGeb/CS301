2e9d18774f4e34f2f82501b2c6317de45087942e
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-02-26 10:49:23 -0500

    Update prompt.js

--- w1d2/prompt.js
+++ w1d2/prompt.js
@@ -5,6 +5,6 @@ const prompt = require("prompt-sync")();
 // let x = prompt("How old are you? ");
 // console.log(x);
 
-let tempInCelsius = prompt("Eneter value in celsius: ");
+let tempInCelsius = prompt("Enter value in celsius: ");
 let tempInFahrenheit = (9 / 5) * parseFloat(tempInCelsius) + 32;
 console.log(tempInFahrenheit);
