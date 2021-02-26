52db1bd35e2ba850c226239e6fee03d7b509cf6c
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-02-25 20:41:52 -0500

    ASSIGNMENT

---
+++ w1d4/patternsNestedLoop.js
@@ -0,0 +1,23 @@
+// for (let i = 1; i < 6; i++) {
+//   let row = "";
+//   for (let j = 0; j < i; j++) {
+//     row += i + "";
+//   }
+//   console.log(row);
+// }
+
+// for (let i = 1; i < 6; i++) {
+//   let row = "";
+//   for (let j = 1; j <= i; j++) {
+//     row += j + "";
+//   }
+//   console.log(row);
+// }
+
+for (let i = 5; i > 0; i--) {
+  let row = "";
+  for (let j = i; j > 0; j--) {
+    row += i + "";
+  }
+  console.log(row);
+}
