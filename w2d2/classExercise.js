9fde23823ce4981a066efc2db942a52243114d5b
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-03-02 17:51:29 -0500

    update

--- w2d2/classExercise.js
+++ w2d2/classExercise.js
@@ -6,14 +6,25 @@
 
 // console.log(isEven(10));
 
-function triangleArea(a, b, c) {
-  let s = semiPerimeter(1, 2, 3);
-  let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-  return area;
-}
+// function triangleArea(a, b, c) {
+//   let s = semiPerimeter(1, 2, 3);
+//   let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+//   return area;
+// }
 
-function semiPerimeter(x, y, z) {
-  let semiPerimeter = (x + y + z) / 2;
-  return semiPerimeter;
+// function semiPerimeter(x, y, z) {
+//   let semiPerimeter = (x + y + z) / 2;
+//   return semiPerimeter;
+// }
+// console.log(triangleArea(1, 2, 3));
+
+function b() {
+  function a() {
+    console.log(x);
+  }
+  x = 5;
+  a();
+  console.log(x);
 }
-console.log(triangleArea(1, 2, 3));
+let x = 20;
+b();
