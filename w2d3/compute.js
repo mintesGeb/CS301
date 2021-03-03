be5119504871de5f9ad0b30a1e626523498694e9
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-03-03 14:31:37 -0500

    update

--- w2d3/compute.js
+++ w2d3/compute.js
@@ -1,11 +1,31 @@
 "use strict";
 
-const compute = function (add, num1, num2) {
-  return add(num1, num2);
+// const compute = function (add, num1, num2) {
+//   return add(num1, num2);
+// };
+
+// function add(n1, n2) {
+//   return n1 + n2;
+// }
+
+// console.log(compute(add, 2, 3));
+// const compute = function (add, num1, num2) {
+//   return add(num1, num2);
+// };
+
+// function add(n1, n2) {
+//   return n1 + n2;
+// }
+
+let compute = function (add, x, y) {
+  return add(x, y);
 };
-
-function add(n1, n2) {
-  return n1 + n2;
-}
-
-console.log(compute(add, 2, 3));
+console.log(
+  compute(
+    function (n1, n2) {
+      return n1 + n2;
+    },
+    2,
+    5
+  )
+);
