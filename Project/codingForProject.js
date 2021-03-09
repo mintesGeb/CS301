5478c390b9c1346a626fc6a3251243affcc4d827
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-03-08 19:12:23 -0500

    Project code

--- Project/codingForProject.js
+++ Project/codingForProject.js
@@ -1,4 +1,7 @@
 "use strict";
+// eslint-disable;
+
+// ?---------- 1 --- not done
 /**
  *
  * @param {number} num check if a number is prime
@@ -16,7 +19,7 @@ function checkPrime(num) {
   }
   return isPrime;
 }
-console.log(checkPrime(49));
+// console.log(checkPrime(49));
 
 /**
  *
@@ -29,14 +32,101 @@ function endsIn9(num) {
   if (lastDigit !== 9) return (endsInNine = false);
   return endsInNine;
 }
-console.log(endsIn9(139));
+// console.log(endsIn9(139));
 
-function findPorcupineNumber(num) {
-  if (endsIn9(num)) {
-    for (let i = num + 1; i > num; i++) {
-      if (endsIn9(i) && checkPrime(i)) return i;
+function nextPrimeNumEndingIn9(num) {
+  let arr = [];
+  for (let i = num + 1; i < 150; i++) {
+    if (checkPrime(i) && endsIn9(i)) {
+      arr.push(i);
     }
   }
-  return -1;
+  return arr;
 }
-console.log(findPorcupineNumber(39));
+// console.log(nextPrimeNumEndingIn9(50));
+
+// function countPrimesInBetween(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = arr[i]; j < arr[i + 1]; j++) {
+//       if (checkPrime(j)) break;
+//     }
+//   }
+// }
+
+/**
+ *
+ * @param {number} num number to start from while finding the next porcupine
+ * @returns {number} arr[0] the porcupine number
+ */
+function findPorcupineNumber(num) {}
+// console.log(findPorcupineNumber(39));
+
+// ?---------- 2 ---- done
+
+function addToTarget(arr, num) {
+  let array = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] + arr[i + 1] === num) {
+      array.push(i);
+      array.push(i + 1);
+    }
+  }
+  return array;
+}
+// console.log(addToTarget([2, 7, 11, 15], 9));
+
+// ?---------- 3 ----  done
+
+function mostFrequent(arr) {
+  let maxChar;
+  let max = 0;
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    count = 0;
+    for (let each of arr) {
+      if (arr[i] === each) {
+        count++;
+      }
+    }
+    if (count > max) {
+      max = count;
+      maxChar = arr[i];
+    }
+  }
+  return maxChar;
+}
+// console.log(mostFrequent([4, 3, 6, 67, 8, 3, 6, 8, 4, 6, 9, 3, 6, 4]));
+
+// ?---------- 4 ---- not done --- recheck
+
+function closestToZero(arr) {
+  let array = [];
+  let min = Infinity;
+  let max = -Infinity;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      let sum = arr[i] + arr[j];
+      if (sum === 0) {
+        array[0] = arr[i];
+        array[1] = arr[j];
+        return array;
+      } else if (sum > 0) {
+        if (sum < min) {
+          min = sum;
+          array[0] = arr[i];
+          array[1] = arr[j];
+        }
+      } else {
+        if (Math.abs(sum) < min) {
+          min = sum;
+          array[0] = arr[i];
+          array[1] = arr[j];
+        }
+      }
+    }
+  }
+  return array;
+}
+console.log(closestToZero([4, -1, -2, -3]));
